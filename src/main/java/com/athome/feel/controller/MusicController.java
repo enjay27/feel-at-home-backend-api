07bfa74a019216d21f81040f8c16b8f75e51c3c6
권기석 <soorison@naver.com>
2021-11-25 12:40:09 +0900

    Music like 기능 추가중

--- src/main/java/com/athome/feel/controller/MusicController.java
+++ src/main/java/com/athome/feel/controller/MusicController.java
@@ -1,6 +1,7 @@
 package com.athome.feel.controller;
 
 import com.athome.feel.model.MusicDto;
+import com.athome.feel.model.SonglikeDto;
 import com.athome.feel.model.service.MusicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +19,8 @@ public class MusicController {
 
     @PostMapping("/recommend")
     public ResponseEntity<?> recommend(@RequestBody List<MusicDto> musicDtos) {
-        musicService.addMusicIfAbsent(musicDtos);
-        return ResponseEntity.ok().build();
+        List<MusicDto> result = musicService.addMusicIfAbsent(musicDtos);
+        return ResponseEntity.ok(result);
     }
 
     // song_like 테이블 만들고
@@ -27,4 +28,16 @@ public class MusicController {
     // PostMapping - song_id, member_id 받아서 db 에 저장
     // @PostMapping("/url")
     //
+    
+    @PostMapping("/url")
+    public ResponseEntity<?> like(@RequestBody SonglikeDto songlikeDto) {
+    	musicService.likeMusic(songlikeDto);
+    	return ResponseEntity.ok().build();
+    }
+    
+    @DeleteMapping("/url")
+    public ResponseEntity<?> dislike(@RequestBody SonglikeDto songlikeDto){
+    	musicService.dislikeMusic(songlikeDto);
+    	return ResponseEntity.ok().build();
+    }
 }
